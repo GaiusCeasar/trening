e603f77a371e8b3917578377148ce3f97acef1e5
Maksim <ivakmaksim@yandex.ru>
2021-12-21 17:34:04 +0300

    added new

--- astlab.py
+++ astlab.py
@@ -3,7 +3,7 @@ import requests
 
 class ASTLab:
     def __init__(self):
-        self.base_url = "http://ipbackend-master.test.fast-system.ru/"
+        self.base_url = "http://ipbackend-master.test.fast-system.ru/api/"
 
     def get_token(self, login, password):
 
@@ -11,7 +11,7 @@ class ASTLab:
            'login': login,
            'password': password,
           }
-        res = requests.post(self.base_url + 'api/auth/login', data=body)
+        res = requests.post(self.base_url + 'auth/login', data=body)
         status = res.status_code
         result = ' '
 
@@ -20,3 +20,34 @@ class ASTLab:
         except:
             result = res.text
         return status, result
+
+    def get_all_applications(self, authorization):
+
+        headers = {'Authorization': 'Bearer ' + authorization['token']}
+
+
+        res = requests.get(self.base_url + 'applications', headers=headers)
+        status = res.status_code
+        result = ' '
+
+        try:
+            result = res.json()
+        except:
+            result = res.text
+        return status, result
+
+    def get_application_by_params(self, authorization, fio):
+        headers = {'Authorization': 'Bearer ' + authorization['token']}
+        filters = {'fio': fio
+                  }
+        res = requests.get(self.base_url + 'applications', headers=headers, params=filters)
+        status = res.status_code
+        result = ' '
+        #print(res.url)
+
+        try:
+            result = res.json()
+        except:
+            result = res.text
+        return status, result
+
--- tests/testAST.py
+++ tests/testAST.py
@@ -1,5 +1,6 @@
 from astlab import ASTLab
 from settingast import valid_login, valid_password
+from pytest_html.extras import json
 
 pf = ASTLab()
 
@@ -8,4 +9,20 @@ def test_get_api_key_valid_user(login=valid_login, password=valid_password):
     status, result = pf.get_token(login, password)
     assert status == 200
     assert 'token' in result
+    # print(result)
+
+
+def test_get_all_applications():
+    _, authorization = pf.get_token(valid_login, valid_password)
+    status, result = pf.get_all_applications(authorization)
+    assert status == 200
+    assert len(result['data']) > 0
+    print(json(result))
+
+
+def test_get_applications_by_params(fio='Марщал Олег Яковлевич'):
+    _, authorization = pf.get_token(valid_login, valid_password)
+    status, result = pf.get_application_by_params(authorization, fio)
     print(result)
+    assert status == 200
+    assert result['Марщал Олег Яковлевич'] == fio
