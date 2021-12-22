a40d06287726a4fd476b8ec11f0734fade20080d
Maksim <ivakmaksim@yandex.ru>
2021-12-22 17:35:23 +0300

    added new

--- astlab.py
+++ astlab.py
@@ -8,9 +8,9 @@ class ASTLab:
     def get_token(self, login, password):
 
         body = {
-           'login': login,
-           'password': password,
-          }
+            'login': login,
+            'password': password,
+        }
         res = requests.post(self.base_url + 'auth/login', data=body)
         status = res.status_code
         result = ' '
@@ -25,7 +25,6 @@ class ASTLab:
 
         headers = {'Authorization': 'Bearer ' + authorization['token']}
 
-
         res = requests.get(self.base_url + 'applications', headers=headers)
         status = res.status_code
         result = ' '
@@ -37,17 +36,17 @@ class ASTLab:
         return status, result
 
     def get_application_by_params(self, authorization, fio):
-        headers = {'Authorization': 'Bearer ' + authorization['token']}
         filters = {'fio': fio
-                  }
+                   }
+        headers = {'Authorization': 'Bearer ' + authorization['token']}
+
         res = requests.get(self.base_url + 'applications', headers=headers, params=filters)
         status = res.status_code
         result = ' '
-        #print(res.url)
+        print(res.url)
 
         try:
             result = res.json()
         except:
             result = res.text
         return status, result
-
--- tests/testAST.py
+++ tests/testAST.py
@@ -9,7 +9,7 @@ def test_get_api_key_valid_user(login=valid_login, password=valid_password):
     status, result = pf.get_token(login, password)
     assert status == 200
     assert 'token' in result
-    # print(result)
+    print(result)
 
 
 def test_get_all_applications():
@@ -20,9 +20,14 @@ def test_get_all_applications():
     print(json(result))
 
 
-def test_get_applications_by_params(fio='Марщал Олег Яковлевич'):
+def test_get_applications_by_params(fio='Хлопов Антон Сергеевич'):
     _, authorization = pf.get_token(valid_login, valid_password)
     status, result = pf.get_application_by_params(authorization, fio)
     print(result)
+    test_client = result['data'][0]['client']
+    test_fio = test_client['lastName'] + ' ' + test_client['firstName'] + ' ' + test_client['middleName']
     assert status == 200
-    assert result['Марщал Олег Яковлевич'] == fio
+    assert len(result['data']) == 1
+    assert fio == test_fio
+    print(test_fio)
+
